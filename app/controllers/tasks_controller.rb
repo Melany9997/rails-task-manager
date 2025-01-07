d79e51affaffe4c95c10f677019b9a3e1934e397
Melany Sierra <melanysabrina@icloud.com>
2025-01-07 16:53:47 +0000

    done some questions

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,20 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks =  Task.all
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @tasks = Task.new(params[:task])
+  @tasks.save
+  end
+
+end
